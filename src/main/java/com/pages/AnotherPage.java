453c551a385618e07797d7ac2e5450c00cc724f9
Mishu <mihai@mihai>
2015-04-27 21:12:11 +0300

    dssdsd

---
+++ src/main/java/com/pages/AnotherPage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+import net.thucydides.core.annotations.findby.FindBy;
+import net.thucydides.core.pages.WebElementFacade;
+
+public class AnotherPage {
+	
+	   @FindBy(name="search")
+	    private WebElementFacade searchTerms;
+
+}
